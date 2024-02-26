b36489acb2066c4943767deed0447db4bfd8339c
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2024-02-26 19:41:29 +0100

    fixed color selection

--- src/components/Boxes/Color.jsx
+++ src/components/Boxes/Color.jsx
@@ -1,16 +1,19 @@
-export default function Color({ color }) {
+export default function Color({ color, selected, setSelected }) {
   const containerStyle = {
     width: "29px",
     height: "29px",
     borderRadius: "50%",
     border: "1px solid lightgray",
     backgroundColor: color || "transparent",
+    boxShadow:
+      selected === color ? "0 0 0 4px #821232, 0 0 0 8px #2a4746" : "none",
   };
   if (color) {
     return (
       <div
+        onClick={() => setSelected(color)}
         style={containerStyle}
-        className={`cursor-pointer hover:scale-110 hover:border-2 hover:border-black hover:shadow-[inset_0px_0px_0px_4px_#821232,0px_0px_0px_4px_#2a4746]`}
+        className={`cursor-pointer hover:scale-110 hover:border-2 hover:border-black`}
       ></div>
     );
   }
--- src/components/ProductColors.jsx
+++ src/components/ProductColors.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import Color from "./Boxes/Color";
 export default function ProductColors({ colors }) {
+  const [selected, setSelected] = useState("#97715B");
   return (
     <div className="w-full md:w-[450px] lg:w-[600px] xl:w-[500px]">
       <div className="flex gap-5 pb-4 pt-7 xl:gap-40 xl:pb-0">
@@ -8,7 +10,12 @@ export default function ProductColors({ colors }) {
       </div>
       <div className="flex flex-wrap gap-3 xl:max-w-[380px] xl:gap-[10px] xl:pt-5">
         {colors.map((item, index) => (
-          <Color key={index} color={item} />
+          <Color
+            key={index}
+            color={item}
+            selected={selected}
+            setSelected={setSelected}
+          />
         ))}
       </div>
     </div>
