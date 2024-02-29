a8ed8cfbaa4274e029fcd6005b3da712dc6f9421
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2024-02-29 17:54:19 +0100

    final result

--- src/components/Boxes/DemoPopup.jsx
+++ src/components/Boxes/DemoPopup.jsx
@@ -1,5 +1,4 @@
 export default function DemoPopup() {
-  console.log("Popup show");
   return (
     <div className="fixed bottom-0 z-50 flex w-[100vw] justify-center py-8">
       <span className=" rounded-xl border border-[#2A4746] bg-[#2A4746] px-2 py-1 text-[16px] text-white">
